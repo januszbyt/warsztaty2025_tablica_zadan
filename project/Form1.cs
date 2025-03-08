e3f5dd7e7eb8390dad38a7e7cc34bc9dac6d81b8
User <User@LAPTOP>
2025-03-08 09:21:39 +0100

    XD

--- project/Form1.cs
+++ project/Form1.cs
@@ -12,9 +12,15 @@ namespace project
 {
     public partial class Form1 : Form
     {
+        int xd = 1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
